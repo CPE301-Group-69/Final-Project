d679b23ce70d5b3bd71bcd142b264b0928f92f95
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-04-17 11:40:57 -0700

    Update 301_DC_motorRun.ino

--- 301_DC_motorRun.ino
+++ 301_DC_motorRun.ino
@@ -5,18 +5,12 @@ volatile unsigned char *portE =    (unsigned char *) 0x2e;
 // int dir2=3;
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  // pinMode(dir1,OUTPUT);
-  // pinMode(dir2,OUTPUT);
-  *portDDRE |= 0x01 << 5;//sets pinB3(dir2) to OUTPUT
-  *portDDRE |= 0x01 << 4;//sets pinB4(dir1) to OUTPUT
+  *portDDRE |= 0x01 << 5;//sets pin2 (IN1) to OUTPUT
+  *portDDRE |= 0x01 << 4;//sets pin3 (IN2) to OUTPUT
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // digitalWrite(3,LOW);
-  // digitalWrite(4,HIGH);
-  *portE &= ~(0x01 << 5); // dir
-  *portE |= 0x01<< 4;
+  *portE &= ~(0x01 << 5); //set pin 2 low 
+  *portE |= 0x01<< 4; //set pin 3 high
 }
