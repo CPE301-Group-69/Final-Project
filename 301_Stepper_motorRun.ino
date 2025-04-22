d6943ef63e7d7e4826ee30b0a48857519361f860
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-04-22 11:34:40 -0700

    Update 301_Stepper_motorRun.ino
    added documentation for pin connections

--- 301_Stepper_motorRun.ino
+++ 301_Stepper_motorRun.ino
@@ -1,7 +1,13 @@
 #include <Stepper.h>
-//connect IN1 - IN4 on driver to desired pins
+/*
+connect pins as follows:
+  IN1 - pin 20
+  IN2 - pin 21
+  IN3 - pin 22
+  IN4 - pin 23
+*/
 
-//steps per rotation
+//steps per rotation, currently set to rotate vent 180 degrees @ speed 15
 const int stepsPerRevolution = 955;
 //pins in order IN1, IN3, IN2, IN4
 Stepper myStepper = Stepper(stepsPerRevolution, 20, 22, 21, 23); //change to whatever desired pins are
