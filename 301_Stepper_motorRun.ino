8f6fc1fe52c2ba551bf49615f8327616e4fa3f30
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-04-22 11:16:59 -0700

    stepper moter functions working

---
+++ 301_Stepper_motorRun.ino
@@ -0,0 +1,30 @@
+#include <Stepper.h>
+//connect IN1 - IN4 on driver to desired pins
+
+//steps per rotation
+const int stepsPerRevolution = 955;
+//pins in order IN1, IN3, IN2, IN4
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11); //change to whatever desired pins are
+
+void setup() {
+  
+}
+
+void loop() {
+  openVent();
+  closeVent();
+}
+
+void openVent(){
+  myStepper.setSpeed(15);
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+  myStepper.step(0);
+}
+
+void closeVent(){
+  myStepper.setSpeed(15);
+  myStepper.step(-stepsPerRevolution);
+  delay(1000);
+  myStepper.step(0);
+}
